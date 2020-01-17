8a26306abdf5412a2829517378e972d19df282c6
Victor Sp <greatgomer2@gmail.com>
2020-01-17 17:07:51 +0300

    Check commit

--- app/src/main/java/one/man/army/ResultClass.java
+++ app/src/main/java/one/man/army/ResultClass.java
@@ -1,6 +1,6 @@
 package one.man.army;
 
-public class ResultClass{                                                                               //Реализация обратной польской анотации.
+public class ResultClass{                                                                               //Реализация обратной польской анотации. Проверка.
     public void Alhorithm(String sourseExpression){
 
     }
